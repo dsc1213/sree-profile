5580b7d452d4d5258024bc8dbc4730fac9064455
Sree Charan Damodara <sreecharan.damodara@gmail.com>
2018-10-14 23:08:00 -0700

    create page layout

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-const App = () =>{
-  return (
-    <div>
-      <h1>Hello, Welcome !!</h1>
-    </div>
-  )
-};
+import App from "./src/app";
+
 render(<App/>, document.getElementById("app"));
---
+++ src/components/Side/side.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import './side.css';
+
+export default class Side extends PureComponent {
+  render() {
+    return (
+      <div className="side">
+        <h1>Hello, this is Side</h1>
+      </div>
+    )
+  }
+}
